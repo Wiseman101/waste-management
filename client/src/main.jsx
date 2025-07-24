a0e3c9b46edcd06f4001e47c484ffd81f1a92cdb
Wiseman101 <hillarywafula45@gmail.com>
2025-07-24 11:21:31 -0700

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import './index.css';
-import './app.css';
+import './App.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
